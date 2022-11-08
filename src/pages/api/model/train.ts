da44905ee47d212773096ea1fde0bb42c03409ce
Damian Świstowski <damian@swistowski.org>
2022-11-08 13:35:51 +0000

    fix api

--- src/pages/api/model/train.ts
+++ src/pages/api/model/train.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { prisma } from "../../../server/db/client";
 import { ModelClass, modelClasses, ModelName, parentModels } from "../../../utils/consts";
+import { photoUrl } from "../../../utils/helpers";
 
 const getOldestCreatedModel = async () => {
   const model = await prisma.model.findFirst({
@@ -18,13 +19,42 @@ const getOldestCreatedModel = async () => {
     orderBy: { created: "desc" }
   })
   if(!model) return null;
-  console.log(modelClasses.get(model.parent_model_code as ModelClass),modelClasses,  model.parent_model_code)
+  const parentModel = parentModels.get(model.parent_model_code as ModelName)
+  if(!parentModel) return null;
   return {
-    ...model, parent_model: parentModels.get(model.parent_model_code as ModelName),
+    ...model, parent_model: parentModel
   };
 }
 
-const getModel = async ():Promise< Awaited<ReturnType<typeof  getOldestCreatedModel>>  | null> => {
+interface GenerateRegularization {
+  type: "generate"
+  prompt: string
+  count: number
+}
+
+interface FetchRegularization {
+  type: "fetch"
+  source: string
+}
+
+interface GetModelResponse {
+  id: string;
+  name: string;
+  owner_id: string;
+  parent_model_code: string;
+  regularization: GenerateRegularization | FetchRegularization,
+  subject: {
+    slug: string
+    subject_photos: string[]
+  }
+  parent_model: {
+    repo_id: string
+    filename: string
+  }
+}
+
+
+const getModel = async (): Promise<GetModelResponse  | null> => {
   const model = await getOldestCreatedModel();
   if(model === null) return null;
 
@@ -39,7 +69,22 @@ const getModel = async ():Promise< Awaited<ReturnType<typeof  getOldestCreatedMo
   })
   // returned by somebody else
   if(updateCount.count === 0) return await getModel();
-  return model;
+
+  return {
+    id: model.id,
+    name: model.name,
+    owner_id: model.owner_id,
+    parent_model_code: model.parent_model_code,
+    regularization: model.regularization as unknown as GenerateRegularization | FetchRegularization,
+    subject: {
+      slug: model.subject.slug,
+      subject_photos: model.subject.subject_photos.map((photo) => photoUrl(photo))
+    },
+    parent_model: {
+      repo_id: model.parent_model.repoId,
+      filename: model.parent_model.filename
+    }
+  }
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
