d785a4a93fa53e8d4c615de093818d9cf4aba789
Damian Świstowski <damian@swistowski.org>
2022-11-20 03:35:40 +0000

    fix

--- src/pages/prompt/[id].tsx
+++ src/pages/prompt/[id].tsx
@@ -4,25 +4,34 @@ import { NextPage } from "next";
 import { Layout } from "../../components/Layout";
 import useStyle from "../../hooks/useStyle";
 import { trpc } from "../../utils/trpc";
-import Photo from "../../components/Photo";
-import Breadcrumbs from "../../components/Breadcrumbs";
-import GeneratePhotos from "../../components/GeneratePhotos";
-import { toast } from "react-hot-toast";
-import { PhotosGrid } from "../../components/PhotosGrid";
 import H2 from "../../components/H2";
+import PhotosGrid from "../../components/PhotosGrid";
+import Navigation from "../../components/Navigation";
+import useNavigation from "../../hooks/useNavigation";
+import usePhotos from "../../hooks/usePhotos";
 
 const ParentModelByCode: NextPage = () => {
+  const navigation = useNavigation();
+
   const router = useRouter();
   const promptId = router.query.id as string;
   const { data: prompt, isError } = trpc.prompt.get.useQuery(promptId, {
     enabled: !!promptId,
   });
+  const { data: photos } = usePhotos(
+    { ...navigation, promptId: promptId },
+    {
+      enabled: !!promptId,
+    }
+  );
   console.log(!!promptId);
   if (!prompt) return <Layout>Loading... {JSON.stringify(prompt)}</Layout>;
   return (
     <Layout>
       <H2>{prompt.content}</H2>
-      <PhotosGrid promptId={promptId} />
+      <PhotosGrid photos={photos}>
+        <Navigation {...navigation} />
+      </PhotosGrid>
     </Layout>
   );
 };
