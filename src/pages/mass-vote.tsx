793bb38786f7eb1aa65e18484499db8d8f00b794
Damian Świstowski <damian@swistowski.org>
2022-11-21 01:07:42 +0000

    add internal vote page

---
+++ src/pages/mass-vote.tsx
@@ -0,0 +1,64 @@
+import type { NextPage } from "next";
+import { Layout } from "../components/Layout";
+import React, { useEffect } from "react";
+import { trpc } from "../utils/trpc";
+import { type Photo as PhotoModel } from "@prisma/client";
+import Photo from "../components/Photo";
+import Button from "../components/Button";
+
+const ranking = ["💩", "🤷🏼‍♂️", "👌", "❤️", "🔥"];
+
+const MassVote: NextPage = () => {
+  const [photos, setPhotos] = React.useState<PhotoModel[]>([]);
+  const voting = trpc.photos.vote.useMutation({
+    onSuccess: (data) => {
+      console.log("data", data);
+      if (data) {
+        setPhotos((photos) => photos.filter((p) => p.id !== data.id));
+      }
+    },
+  });
+  const { refetch, isLoading } = trpc.photos.toVote.useQuery(
+    {
+      count: 5,
+      maxVotes: 1,
+    },
+    {
+      staleTime: Infinity,
+      onSuccess: (data) => {
+        setPhotos(data);
+      },
+    }
+  );
+  useEffect(() => {
+    if (photos.length === 0) refetch();
+  }, [photos]);
+  return (
+    <Layout>
+      {photos[0] && (
+        <div className="m-auto flex w-fit flex-col items-center p-2">
+          <div className="w-fit w-[512px]">{photos[0].prompt}</div>
+          <Photo photo={photos[0]} />
+          <div className="flex w-full justify-between py-2">
+            {ranking.map((v, i) => (
+              <Button
+                disabled={voting.isLoading || isLoading}
+                onClick={() => {
+                  voting.mutate({
+                    id: photos[0]?.id || "",
+                    vote: i,
+                  });
+                }}
+                key={i}
+              >
+                {v}
+              </Button>
+            ))}
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+export default MassVote;
