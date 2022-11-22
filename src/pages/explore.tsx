2b6ed194d5408bc97bdd5e894b2464b7dc322e6d
Damian Świstowski <damian@swistowski.org>
2022-11-22 20:18:07 +0000

    make index page empty (#13)
    * make index page empty
    
    * make index page empty

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
 import React from "react";
+import PhotosGrid from "../components/PhotosGrid";
 import H1 from "../components/H1";
-import { useSession } from "next-auth/react";
+import usePageScrollPhotos from "../hooks/usePageScrollPhotos";
 
-const Home: NextPage = () => {
-  const session = useSession();
+const Explore: NextPage = () => {
+  const { data: photos } = usePageScrollPhotos();
 
   return (
     <>
@@ -19,15 +20,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <H1 className="my-4">Hi, I&apos;m Bob!</H1>
-        {session.data?.user ? (
-          <div>Logged user: {session.data.user?.name}</div>
-        ) : (
-          <div>Not logged in</div>
-        )}
+        <H1 className="my-4">
+          Here is my recent work I&apos;m really proud of, feel free to look
+        </H1>
+        <PhotosGrid photos={photos}></PhotosGrid>
       </Layout>
     </>
   );
 };
 
-export default Home;
+export default Explore;
